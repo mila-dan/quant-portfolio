79c786c539a826e6b33af190ede86753051e9446
mila-dan <mila.danilovskaya@gmail.com>
2026-01-05 21:45:19 +0000

    Added Markowitz mean-variance optimization with CVXPY

--- portfolio_optimization/mean_variance.py
+++ portfolio_optimization/mean_variance.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cvxpy as cp
 
 def portfolio_return(weights, mean_returns):
     return np.dot(weights, mean_returns)
@@ -12,3 +13,27 @@ def sharpe_ratio(weights, mean_returns, cov_matrix, risk_free_rate=0.0425):
     p_return = portfolio_return(weights, mean_returns)
     p_volatility = portfolio_volatility(weights, cov_matrix)
     return (p_return - risk_free_rate) / p_volatility
+
+def optimise_portfolio(mean_returns, cov_matrix, target_return):
+    mean_returns = np.asarray(mean_returns)
+    cov_matrix = np.asarray(cov_matrix)
+
+    #define the unknown variable x : f(x) . THis creates a vector of variables w = (w1, w2, ..., wn)
+    weights = cp.Variable(len(mean_returns))
+
+
+    # objective - find the portfolio weights that minimize portfolio variance
+
+
+    objective = cp.Minimize(cp.quad_form(weights, cov_matrix)) 
+
+    constraints = [
+    cp.sum(weights) == 1,
+    mean_returns @ weights >= target_return,
+    weights >= 0
+    ]
+
+    problem = cp.Problem(objective, constraints)
+    problem.solve()
+
+    return np.round(weights.value, 6)
